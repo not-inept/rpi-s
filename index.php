c9d6f52829926dc54621fa0830fcb0e1df72f0ae
Matthew Hancock <mbhancock@me.com>
2014-11-13 17:28:37 -0500

    Basic Structure

--- index.php
+++ index.php
@@ -6,7 +6,28 @@
 </head>
 
 <body>
-	
+	<div class="container">
+		<?php include 'resources/navigation.php' ?>
+		
+		<section class="left inline">
+			<div id="map">
+			</div>
+		</section>
+
+		<section class="right inline">
+			<form id="loginForm" class="border" action="login.php" method="post">
+				<input type="text" name="username" placeholder="Username"/>
+				<input type="password" name="password" placeholder="Password"/>
+				<input type="submit" value="LOGIN"/>
+				<input type="button" value="Sign Up"/>
+			</form>
+			<section class="news border">
+				<h2>News Events</h2>
+				<div id="events">
+				</div>
+			</section>
+		</section>
+	</div>
 </body>
 
 <!-- improves performance by moving script files to bottom -->
---
+++ resources/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+	<span class="title">RPI Survival Game</span>
+	<ul>
+		<li><a href="contact.php">Contact Us</a></li>
+		<li><a href="controls.php">Controls</a></li>
+		<li><a href="rules.php">Rules</a></li>
+	</ul>
+</nav>
