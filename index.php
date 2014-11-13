7120e7722ac9633cd84620e74ac328dc9f629dfa
Matthew Hancock <mbhancock@me.com>
2014-11-13 16:18:58 -0500

    Initial File Layout

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RPI Adventure Game</title>
+	<link rel="stylesheet" href="style/main.css" type="text/css"/>
+</head>
+
+<body>
+	
+</body>
+
+<!-- improves performance by moving script files to bottom -->
+<script type="text/javascript" src="scripts/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="scripts/main.js"></script>
+
+</html>
