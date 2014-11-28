1f2c9f242eeaa08838851deb6a7d47197e8033f2
guoy8 <guoy8@rpi.edu>
2014-11-28 13:01:45 -0500

    Update navigation.php

--- resources/navigation.php
+++ resources/navigation.php
@@ -3,10 +3,17 @@
 		RPI Survival Game
 		<sub id="subtitle">A Post-Apocalyptic Parody of Campus</sub>
 	</span>
-	<ul>
-		<li><a href="about.php">About Us</a></li>
+	<ul class = "navigation">		
+		<li>
+			<a href="" >About</a>
+		  	<ul>
+		    <li><a href="aboutGame.php">The Game</a></li>
+		    <li><a href="team.php">The Team</a></li>
+		  </ul>
+		</li>
 		<li><a href="controls.php">Controls</a></li>
-		<li><a href="rules.php">Rules</a></li>
 		<li><a href="index.php">Home</a></li>
 	</ul>
+<script type="text/javascript" src="scripts/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="resources/aboutNavigation.js"></script>
 </nav>
