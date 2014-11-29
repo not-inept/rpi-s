f603338e400aa79f97930cba3770ae95ff048f2d
w-eye <coops58@gmail.com>
2014-11-29 18:53:24 -0500

    More shifting to see if dev server changes and works _----_

--- resources/auth/login.php
+++ resources/auth/login.php
@@ -3,13 +3,13 @@
     include_once("./CAS-1.3.2/CAS.php");
     phpCAS::client(CAS_VERSION_2_0,'cas-auth.rpi.edu',443,'/cas/');
     // SSL!
-    //phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
-    phpCAS::setNoCasServerValidation();
-    if (!phpCAS::isAuthenticated())
+    phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
+    //phpCAS::setNoCasServerValidation();
+    if (!phpCAS::checkAuthenticated())
     {
         echo "Testing...";
         phpCAS::forceAuthentication();
-        echo "What is going on...."
+        //echo "What is going on...."
     }else{
         echo "Else..";
         header('location: ../../index.php');
