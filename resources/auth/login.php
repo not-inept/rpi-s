fdc6439f40a5e58d0d2ebe5550f37d5844de8965
w-eye <coops58@gmail.com>
2014-11-29 18:57:28 -0500

    Hopefully the last round of things. -cries-

--- resources/auth/login.php
+++ resources/auth/login.php
@@ -1,18 +1,21 @@
 <?php
-    require_once("../includes/config.php");
-    include_once("./CAS-1.3.2/CAS.php");
+    echo "Literally crying...";
+    require("../includes/config.php");
+    require("./CAS-1.3.2/CAS.php");
+    echo "CAS files loaded...";
     phpCAS::client(CAS_VERSION_2_0,'cas-auth.rpi.edu',443,'/cas/');
     // SSL!
     phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
     //phpCAS::setNoCasServerValidation();
+    echo "Entering if....";
     if (!phpCAS::checkAuthenticated())
     {
-        echo "Testing...";
+        echo "Auth = False...";
         phpCAS::forceAuthentication();
         //echo "What is going on...."
     }else{
-        echo "Else..";
+        echo "Auth = True..";
         header('location: ../../index.php');
     }
-    echo "Got to end";
+    echo "Got to end...";
 ?>
