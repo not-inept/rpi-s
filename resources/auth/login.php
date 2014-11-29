87311dce2d83120c17dd776dddd7b4e5abfb3ece
w-eye <coops58@gmail.com>
2014-11-29 18:34:25 -0500

    Changed ssl preferences to reflect what we have in place

--- resources/auth/login.php
+++ resources/auth/login.php
@@ -3,8 +3,8 @@
     include_once("./CAS-1.3.2/CAS.php");
     phpCAS::client(CAS_VERSION_2_0,'cas-auth.rpi.edu',443,'/cas/');
     // SSL!
-    phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
-    
+    //phpCAS::setCasServerCACert("./CACert.pem");//this is relative to the cas client.php file
+    phpCAS::setNoCasServerValidation();
     if (!phpCAS::isAuthenticated())
     {
         phpCAS::forceAuthentication();
