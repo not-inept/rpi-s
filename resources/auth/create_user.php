ccee5d4320634b62e1f2cd758f58554bf5f200d0
Daniel Ben-Chitrit <dibol13@gmail.com>
2014-11-30 18:58:08 -0500

    Added Duplicate User

--- resources/auth/create_user.php
+++ resources/auth/create_user.php
@@ -1,65 +1,88 @@
-<?php
-	if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['faction']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['email']) && !empty($_POST['faction'])) {
-		require '../includes/config.php';
+$user_chk = $dbconn->prepare('SELECT username FROM users WHERE username=:username');
+      $user_chk->execute(array(':username' => $_POST['username']));
+ 
+      if ($user_chk->fetch()) {
+        $msg = "Duplicate username.";
+      }
+      else{
+        $stmt = $dbconn->prepare("INSERT INTO users (username, password, is_admin) 
+                          VALUES (:username, :pass, :isadmin)");
+        $stmt->execute(array(':username' => $_POST['username'], 
+                           ':pass' => $_POST['pass'],
+                           ':isadmin'=> $admin
+                            ));
+        $msg = "Account created.";
+      }
 
-		$name = $config['DB_NAME']; //DB We're using from config
+      <?php
+  if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['faction']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['email']) && !empty($_POST['faction'])) {
+    require '../includes/config.php';
+    $name = $config['DB_NAME']; //DB We're using from config
+    $username = $_POST['username']; //Data received from form
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $faction = $_POST['faction'];
+    //TODO: Add email verification.
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=$name", $config['DB_USERNAME'], $config['DB_PASSWORD']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
 
-		$username = $_POST['username']; //Data received from form
-		$password = $_POST['password'];
-		$email = $_POST['email'];
-		$faction = $_POST['faction'];
-		//TODO: Add email verification.
-		try {
-		    $pdo = new PDO("mysql:host=localhost;dbname=$name", $config['DB_USERNAME'], $config['DB_PASSWORD']);
-		    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		    try {
-		    	$prep = $pdo->prepare('INSERT INTO `players` (`name`, `allnaturalseasalt`, `password`,`email`,`faction`) VALUES (:username, :salt, :password, :email, :faction);');
-
-				$salt = hash('sha256',uniqid(mt_rand(), true));
-				$password = hash('sha256',$salt.$password);
-				
-				$prep->execute(array(
-					':username' => htmlspecialchars($username),
-					':salt' => $salt,
-					':password' => $password,
-					':email' => $email,
-					':faction' => $faction
-				));
-				echo "User added.</p>";
-				header('location: login.php');
-			} catch(PDOException $e) {
-				print_r($e);
-				echo "User not added.</p>";
-			}
-		}  catch(PDOException $e) {
-			echo "<p>Database does not yet exist...</p>";
-		}
-	} else {
-		echo "
-			<html>
- 			<head>
- 				<title>RPI-S: create user</title>
- 				<link rel='stylesheet' href='../css/style.css' type='text/css'/>
- 			</head>
-				<form id='newUser' name='newUser' action='create_user.php' method='post'>
-					<p>
-						<label for='username'>Username:</label>
-						<input type='name' id='username' name='username'>
-					</p>
-					<p>
-						<label for='password'>Password:</label>
-						<input type='password' id='password' name='password'>
-					</p>
-					<p>
-						<label for='email'>Email:</label>
-						<input type='email' id='email' name='email'>
-					</p>
-					<p>
-						<label for='faction'>Faction:</label>
-						<input type='faction' id='faction' name='faction'>
-					<p>
-					<p><input type='submit' value='Create User'></p>
-				</form>
-			</html>";
-	}	
+          $user_chk = $dbconn->prepare('SELECT username FROM players WHERE username=:username');
+	  $user_chk->execute(array(':username' => $username));
+ 
+      	  if ($user_chk->fetch()) {
+            echo "Duplicate username.";
+            header('location: create_user.php');
+          }
+      	  else{
+          	$prep = $pdo->prepare('INSERT INTO `players` (`name`, `allnaturalseasalt`, `password`,`email`,`faction`) VALUES (:username, :salt, :password, :email, :faction);');
+        	$salt = hash('sha256',uniqid(mt_rand(), true));
+		$password = hash('sha256',$salt.$password);
+        
+		$prep->execute(array(
+	          ':username' => htmlspecialchars($username),
+	          ':salt' => $salt,
+	          ':password' => $password,
+	          ':email' => $email,
+	          ':faction' => $faction
+		 ));
+        	//echo "User added.</p>";
+        	header('location: login.php');
+	 }
+      } catch(PDOException $e) {
+        print_r($e);
+        echo "User not added.</p>";
+      }
+    }  catch(PDOException $e) {
+      echo "<p>Database does not yet exist...</p>";
+    }
+  } else {
+    echo "
+      <html>
+      <head>
+        <title>RPI-S: create user</title>
+        <link rel='stylesheet' href='../css/style.css' type='text/css'/>
+      </head>
+        <form id='newUser' name='newUser' action='create_user.php' method='post'>
+          <p>
+            <label for='username'>Username:</label>
+            <input type='name' id='username' name='username'>
+          </p>
+          <p>
+            <label for='password'>Password:</label>
+            <input type='password' id='password' name='password'>
+          </p>
+          <p>
+            <label for='email'>Email:</label>
+            <input type='email' id='email' name='email'>
+          </p>
+          <p>
+            <label for='faction'>Faction:</label>
+            <input type='faction' id='faction' name='faction'>
+          <p>
+          <p><input type='submit' value='Create User'></p>
+        </form>
+      </html>";
+  } 
 ?>
