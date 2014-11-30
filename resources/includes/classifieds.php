9c62fa6a4ea899f02c942a006918daa9873ecf7b
Droid5x <mrkblnc1248@gmail.com>
2014-11-30 06:46:46 +0000

    classifieds.php
    Should be working. Maybe.

--- resources/includes/classifieds.php
+++ resources/includes/classifieds.php
@@ -1,11 +1,16 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'].'/resources/includes/connect.php';
 // Grab all of the events that are in the morning mail quests:
-if ( $dbconn->query('SELECT * FROM events WHERE quest = 0;') ){
-	foreach ($dbconn->query('SELECT * FROM events WHERE quest = 0;') as $results){
-		echo '<li>>> '.$results['location'].'</li>';
+$result = $dbconn->query('SELECT * FROM events WHERE quest = 0;');
+if ($result){
+	if ( $result->rowCount() != 0 ){
+		foreach ($dbconn->query('SELECT * FROM events WHERE quest = 0;') as $results){
+			echo '<li>>> '.$results['location'].'</li>';	
+		}
+	} else {
+		echo '<li>>> Sorry, no events to show :\\</li>';
 	}
 } else {
-	echo '>~ ERROR: connection error!';
+	echo '<li>>~ ERROR: could not retrieve events :(</li>';
 }
 ?>
