0ea606371fc67e0b90b2fcf9c8c904f49e48437d
w-eye <coops58@gmail.com>
2014-11-30 01:49:41 -0500

    M for mark, not message, and this commit is right on that.

--- resources/includes/classifieds.php
+++ resources/includes/classifieds.php
@@ -1,5 +1,5 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'].'/resources/includes/connect.php';
+require 'resources/includes/connect.php';
 // Grab all of the events that are in the morning mail quests:
 if ( $dbconn->query('SELECT * FROM events WHERE quest = 0;') ){
 	foreach ($dbconn->query('SELECT * FROM events WHERE quest = 0;') as $results){
