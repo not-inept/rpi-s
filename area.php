46def63f9cba5897de3f503da7ded1ec03b50b4d
w-eye <coops58@gmail.com>
2014-11-30 22:34:23 -0500

    Revised area loc display

--- area.php
+++ area.php
@@ -12,17 +12,16 @@
 	<div id="main">
 
 		<!-- TODO: Change map to individual image, allow navigation out -->
-		<!-- Div closed in php, setting image as background -->
 			
 		<?php
 			$mapString = "<div id='map'";
 			if (isset($_GET['loc'])) {				
 				$loc = explode(",",$_GET['loc']);
 				$mapString += " style='background-image: url(resources/images/maps/";
-				$mapString += string($loc[0]);
+				//$mapString += string($loc[0]);
 				//."/".string($loc[1])."_".string($loc[2]).".png);'>";
 			} else {
-				$mapString += ">Not a valid location! D:";
+				$mapString += "> Not a valid location! D:";
 			}
 			$mapString += "</div>";
 			echo $mapString;
