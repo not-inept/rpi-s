ca53f78387ab50b95c529c2b31307bed43f3971a
w-eye <coops58@gmail.com>
2014-12-01 04:52:30 -0500

    Added error handles, like love handles, but so much more painful to look at

--- resources/includes/actions.php
+++ resources/includes/actions.php
@@ -1,6 +1,7 @@
 <?php
 	if (isset($_GET['eventID'])) {
 		try {
+			require_once("./config.php");
 	        $name = $config['DB_NAME']; //DB We're using from config
 	        $pdo = new PDO("mysql:host=localhost;dbname=$name", $config['DB_USERNAME'], $config['DB_PASSWORD']);
 	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -9,7 +10,7 @@
 	        $result = $events->fetch();
 	        print_r($result);
         }  catch(PDOException $e) {
-        	$replace = "...Error loading event.";
+        	echo $e;
     	}
     }
 ?>
