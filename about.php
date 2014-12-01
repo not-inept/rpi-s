e7ae31a365d1cf9fce1635d532abab24ea25d9b3
guoy8 <guoy8@rpi.edu>
2014-12-01 15:18:17 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Team</title>
+	<link rel="stylesheet" href="resources/css/about.css" type="text/css"/>
+</head>
+
+<body>
+	<div class="container">
+		<?php include './resources/includes/header.php' ?>
+	</div>
+	<div id = "team">
+		<h1>Meet the Team</h1>
+		<li>
+			<span class = "name">Daniel Ben-Chitrit</span>
+		</li>
+		<li>
+			<span class = "name">Mark Blanco</span>
+		</li>
+		<li>
+			<span class = "name">Mason Cooper</span>
+		</li>
+		<li>
+			<span class = "name">Yuting Guo</span>
+		</li>
+		<li>
+			<span class = "name">Matthew Hancock</span>
+		<li>
+			<span class = "name">Nobah Lee</span>
+		</li>
+		<p><a href = "https://github.com/not-inept/rpi-s">On GitHub</a></p>
+	</div>
+</body>
+</html>
