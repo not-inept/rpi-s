dc45d37ddaa04acd036bc5e4e51b5afb2fa0b2a9
Droid5x <mrkblnc1248@gmail.com>
2014-12-01 02:11:07 +0000

    More updates to user.php

--- user.php
+++ user.php
@@ -3,8 +3,7 @@
 <?php
 require 'resources/includes/header.php';
 if (isset($_SESSION['username'])){
-	//$stmt = $dbconn->prepare('SELECT o.* FROM inventory i, objects o WHERE i.playerID=:usernum AND o.objID=i.objID;');
-        $stmt = $dbconn->prepare('SELECT * FROM objects;');
+	$stmt = $dbconn->prepare('SELECT o.* FROM inventory i, objects o WHERE i.playerID=:usernum AND o.objID=i.objID;');
         $stmt->execute(array(
 		':usernum' => $_SESSION['id']
 	));
@@ -18,12 +17,7 @@ if (isset($_SESSION['username'])){
         <ul>
         <?php
         foreach ($stmt as $row){
-                echo $row['name'];
-        }
-        $inventory = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo $inventory['name'];
-        while ($row = $stmt->fetch()){
-                echo $row['n'];
+                echo '<li>'.$row['name'].'</li>';
         }
         ?>
         </ul>
