6f2dcd975c81b4ac951cccf6e92476462daaf098
w-eye <coops58@gmail.com>
2014-12-01 02:26:33 -0500

    Fixed the thing

--- user.php
+++ user.php
@@ -24,12 +24,12 @@ if (isset($_SESSION['username'])){
         </section>
 </body>
 
+<script type="text/javascript" src="resources/js/jquery-2.1.1.min.js"></script>
 <script>
 var uname = "anon";
 if ($('#name').length) {
         var uname = $('#name').html().trim();
 }
 $('#location').html(uname + "@rpi-s: " + "/inventory/");
-}
 </script>
 </html>
