ba315806a5da2e54957d2fd835a338fe35411d43
guoy8 <guoy8@rpi.edu>
2014-12-01 15:27:28 -0500

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,14 @@
+<head>
+	<title>FAQ</title>
+	<link rel="stylesheet" href="resources/css/about.css" type="text/css"/>
+</head>
+
+<body>
+	<div class="container">
+		<?php include './resources/includes/header.php' ?>
+	</div>
+	<div id = "faq">
+		<p> Questions will be updated on the site as they are asked.</p>
+	</div>
+</body>
+</html>
